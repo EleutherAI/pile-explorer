a8d76091d8503cc21c421ef4654d16feb09e1f4d
Ubuntu <ubuntu@ip-172-31-26-49.us-west-1.compute.internal>
2020-12-16 09:20:52 +0000

    Update to add all-option, which reads once from the lmd source, and refactoring. Very slow due to tokenization.

--- gensim_topic_model_trainer.py
+++ gensim_topic_model_trainer.py
@@ -6,7 +6,7 @@ import itertools
 from collections import defaultdict
 from gensim.corpora.dictionary import Dictionary
 from gensim.utils import simple_tokenize
-from gensim.models import LdaModel
+from gensim.models import LdaModel, LdaMulticore
 from slugify import slugify
 
 def chunks(iterable, size=10):
@@ -14,7 +14,7 @@ def chunks(iterable, size=10):
     for first in iterator:
         yield itertools.chain([first], itertools.islice(iterator, size - 1))
 
-component_names = {
+component_list = {
     'StackExchange',
     'PubMed Central',
     'HackerNews',
@@ -36,18 +36,21 @@ component_names = {
     'Bibliotik',
     'OpenSubtitles',
     'YoutubeSubtitles',
-    'NIH ExPorter'
+    'NIH ExPorter',
 }
+
+component_options = component_list | { 'all' }
 
 parser = argparse.ArgumentParser(description='Train topic models on a component.')
 
-parser.add_argument('--chunk_size', type=int, default=2**12,
-                    help='Number of documents in a chunk')
-parser.add_argument('--threshold', type=int, default=2**10, help='Number of documents to run another online batch')
 parser.add_argument('--input_path', required=True, help='Path to Pile split to use for dictionary (validation, ideally)')
 parser.add_argument('--dictionary_file', required=True, help='File name to load the dictionary from')
 parser.add_argument('--num_topics', type=int, default=32, help='Number of topics to use for LDA')
-parser.add_argument('--component', required=True, choices=component_names, help='Component tag for the pile component to run the model on')
+parser.add_argument('--component', required=True, choices=component_options, help='Component tag for the pile component to run the model on')
+parser.add_argument('--chunk_size', type=int, default=2**12,
+                    help='Number of documents in a chunk')
+parser.add_argument('--threshold', type=int, default=2**10, help='Number of documents to run another online batch')
+parser.add_argument('--cores', type=int, default=1, help='Number of CPU cores')
 
 args = parser.parse_args()
 
@@ -57,38 +60,63 @@ COMPONENT = args.component
 N_TOPICS = args.num_topics
 CHUNK_SIZE = args.chunk_size
 THRESHOLD = args.threshold
+CORES = args.cores
+
+if COMPONENT == 'all':
+    components = component_list
+else:
+    components = [COMPONENT]
 
 dictionary = Dictionary.load(dictionary_file)
 
 rdr = lmd.Reader(input_path)
-gnr = rdr.stream_data(get_meta=True)
+stream = rdr.stream_data(get_meta=True)
+gnr = ((dictionary.doc2bow(simple_tokenize(text)), meta['pile_set_name']) for (text, meta) in stream)
 doc_chunks = chunks(gnr, size=CHUNK_SIZE)
 
-component_docs = []
-
-model = None
+docs = defaultdict(list)
+models = defaultdict()
 
 progress = 0
 count = 0
 
 for chunk in doc_chunks:
-    print(f'{count:,} documents processed from {COMPONENT} out of {progress:,} total documents' )
+    if count >= 100_000:
+        break
+    print(f'{count:,} documents trained on out of {progress:,} documents read')
 
-    [component_docs.append(dictionary.doc2bow(simple_tokenize(text))) for (text, meta) in chunk if meta['pile_set_name'] == COMPONENT]
+    if len(components) == 22:
+        [docs[component].append(bow) for (bow, component) in chunk]
+    else:
+        [docs[component].append(bow) for (bow, component) in chunk if component in components]   
 
-    if len(component_docs) > THRESHOLD:
-        if model:
-            #pass
-            model.update(component_docs)
-        else:
-            #print(key)
-            model = LdaModel(component_docs, id2word=dictionary, num_topics=N_TOPICS, iterations=100, chunksize=CHUNK_SIZE / 2)
-        count += len(component_docs)
-        component_docs = []
+    for component in components:
+        if len(docs[component]) > THRESHOLD:
+            if component in models.keys():
+                print(f'Training model for {component} component')
+                models[component].update(docs[component])
+            else:
+                print(f'Initializing model for {component} component')
+                if CORES > 1:
+                    models[component] = LdaModel(docs[component],
+                                         id2word=dictionary,
+                                         num_topics=N_TOPICS,
+                                         iterations=100,
+                                         chunksize=CHUNK_SIZE / 2)
+                else:
+                    models[component] = LdaMulticore(docs[component],
+                                                     id2word=dictionary,
+                                                     num_topics=N_TOPICS,
+                                                     iterations=100,
+                                                     chunksize=CHUNK_SIZE / 2,
+                                                     workers=CORES-1)
+
+            count += len(docs[component])
+            docs[component] = []
 
     progress = progress + CHUNK_SIZE
 
-model.update(component_docs)
-component_docs = []
-
-model.save(slugify(COMPONENT) + '.model.topic')
+for component in component_list:
+    model[component].update(docs[component])
+    model.save(slugify(component) + '.model.topic')
+    docs[component] = None
