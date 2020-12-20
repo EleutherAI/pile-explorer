d996cb9b3c425a09125d476b4acf244df4b3918c
Ubuntu <ubuntu@ip-172-31-26-49.us-west-1.compute.internal>
2020-12-20 08:17:15 +0000

    Updated topic model evaluation to do topic diffs, perplexity, and plots. Uncomment for perplexity and plots.

--- gensim_topic_model_evaluator.py
+++ gensim_topic_model_evaluator.py
@@ -6,13 +6,26 @@ import json
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import fasttext
 
 from math import pow
 from multiprocessing import Pool
 from collections import defaultdict
 from gensim.corpora.dictionary import Dictionary
-from gensim.utils import simple_tokenize
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import remove_stopwords
 from gensim.models import LdaModel, LdaMulticore
+from best_download import download_file
+
+download_file('https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin', 'lid.176.bin', '7e69ec5451bc261cc7844e49e4792a85d7f09c06789ec800fc4a44aec362764e')
+
+langdet = fasttext.load_model("lid.176.bin")
+
+def language(doc):
+    details = langdet.predict(doc.replace('\n', ' '), k=1)
+
+    return details[0][0].replace('__label__', '')
+
 
 component_list = {
     'StackExchange',
@@ -46,7 +59,6 @@ parser = argparse.ArgumentParser(description='Train topic models on a component.
 parser.add_argument('--input_path', required=True, help='Path to Pile split to use for dictionary (validation, ideally)')
 parser.add_argument('--models_path', required=True, help='Path to saved LDA models')
 parser.add_argument('--dictionary_file', required=True, help='File name to load the dictionary from')
-parser.add_argument('--component', required=True, choices=component_options, help='Component tag for the pile component to run the model on')
 parser.add_argument('--chunk_size', type=int, default=2**12,
                     help='Number of documents in a chunk')
 parser.add_argument('--tokenizer_processes', type=int, default=1, help='Number of processes for input tokenization')
@@ -57,15 +69,11 @@ args = parser.parse_args()
 input_path = args.input_path
 models_path = args.models_path
 dictionary_file = args.dictionary_file
-COMPONENT = args.component
 CHUNK_SIZE = args.chunk_size
 TOK_PROCESSES = args.tokenizer_processes
 LDA_PROCESSES = args.lda_processes
 
-if COMPONENT == 'all':
-    components = component_list
-else:
-    components = [COMPONENT]
+components = component_list
 
 dictionary = Dictionary.load(dictionary_file)
 
@@ -76,7 +84,10 @@ def baggify(item):
     text, meta = item
     component = meta['pile_set_name']
     if component in components:
-        bow_or_none = dictionary.doc2bow(simple_tokenize(text))
+        if language(text) == 'en':
+            bow_or_none = dictionary.doc2bow(simple_preprocess(remove_stopwords(text), min_len=1, max_len=50))
+        else:
+            bow_or_none = None
     else:
         bow_or_none = None
     return (bow_or_none, component)
@@ -88,32 +99,55 @@ print("Loading models...")
 for component in components:
     models[component] = LdaMulticore.load(models_path + component + '.model.topic')
 
+# Compute cross-corpus topic similarities    
+    
+diffs = defaultdict(defaultdict)
+
+for (train, test) in itertools.product(*(components, components)):
+    print(f'Computing topic diff for {train} on {test}...')
+    diffs[train][test] = models[train].diff(models[test]).tolist()
+    
+with open('topic_model_diffs.json', 'w') as fp:
+    json.dump(diffs, fp)
+    print("Saving topic modeling cross-corpus diffs...")
+ 
+
+"""
+# Compute cross-corpus perplexities 
+perplexities = defaultdict(defaultdict)
+
 with Pool(TOK_PROCESSES) as p:
     print("Reading documents...")
     doc_iter = p.imap_unordered(baggify, stream, chunksize=CHUNK_SIZE)
     for (i, (bow, component)) in enumerate(doc_iter):
-        #print(f'Read {i:,} documents so far')
         if bow:
-            docs[component].append(bow)        
-
-perplexities = defaultdict(dict)
-
-for (training_corpus, eval_corpus) in itertools.product(*(components, components)):
-    print(f'Computing perlexity for {training_corpus} on {eval_corpus}...')
-    perplexity = pow(2, -models[training_corpus].log_perplexity(docs[eval_corpus]))
-    perplexities[training_corpus][eval_corpus] = perplexity
+            docs[component].append(bow)    
+            
+for (train, test) in itertools.product(*(components, components)):
+    print(f'Computing perplexities for model trained on {train} tested on {test}...')
+    perplexities[train][test] = pow(2, -models[train].log_perplexity(docs[test]))
     
 with open('topic_model_perplexities.json', 'w') as fp:
     json.dump(perplexities, fp)
     print("Saving topic modeling cross-corpus perplexities...")
-    
+ 
 df = pd.DataFrame.from_dict(perplexities,
-                   orient='index')
+                       orient='index')
+df = df.sort_index(axis=0)
+df = df.sort_index(axis=1)
 log_perplexities = df.apply(np.log)
 
-ax = sns.heatmap(log_perplexities, xticklabels=True, yticklabels=True)
-ax.set_xlabel("Testing Corpus")
-ax.set_ylabel("Training Corpus")
+print("Mean perplexities:", log_perplexities.mean(axis=1))
+
+cm = sns.clustermap(log_perplexities.transpose(), dendrogram_ratio=0.33, metric='euclidean', xticklabels=True, yticklabels=True, col_cluster=False, cbar_pos=(0, 0, 0.05, 0.2), tree_kws={ 'linewidths': 4})
+ax = cm.ax_heatmap
+ax.set_xlabel("Training Corpus")
+ax.set_ylabel("Testing Corpus")
 
 fig = ax.get_figure()
 fig.savefig('topic_model_perplexities.png')
+
+# Show topics for each component
+for component in components:
+    print(component, models[component].show_topics(32, 10))
+"""
